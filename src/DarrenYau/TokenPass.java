18e216dad2859f22efef2bd7ff3ff1a91c5d7d87
dyau4570 <43213028+dyau4570@users.noreply.github.com>
2018-12-04 22:29:59 -0500

    Initial commit

---
+++ src/DarrenYau/TokenPass.java
@@ -0,0 +1,52 @@
+package DarrenYau;
+
+import javax.sound.midi.Soundbank;
+
+public class TokenPass {
+    private int[] board;
+    private int currentPlayer;
+
+    public TokenPass(int playerCount) {
+        this.board = new int[playerCount];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = 1 + (int)(Math.random()*10);
+        }
+        currentPlayer = (int) (playerCount * Math.random());
+    }
+
+    public void distributeCurrentPlayerTokens() {
+        int nextPlayer = currentPlayer;
+        int numToDistribute = board[currentPlayer];
+        board[currentPlayer] = 0;
+
+        while(numToDistribute > 0) {
+            nextPlayer = (nextPlayer + 1) % board.length;
+            board[nextPlayer]++;
+            numToDistribute--;
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("Player " + i + ": " + board[i]);
+        }
+        System.out.println("Current Player:" + currentPlayer);
+    }
+
+    public void nextPlayer() {
+        currentPlayer++;
+        if (currentPlayer == board.length) {
+            currentPlayer = 0;
+        }
+    }
+
+    public int gameOver(){
+        int winner = -1;
+        for(int i = 0; i < board.length; i++) {
+            if(board[i] == 0) {
+                winner = i;
+            }
+        }
+        return winner;
+    }
+}
